42ba9c98ba88dd6a639c4796a66b828a1847f178
WMatheus2022 <pachwander4@gmail.com>
2023-09-16 12:11:13 -0300

    desafio-dio-java

---
+++ src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Valor do parametro 1: ");
+        int parametro1 = terminal.nextInt();
+        System.out.println("Valor do parametro 2: ");
+        int parametro2 = terminal.nextInt();
+
+
+        try {
+            contar(parametro1, parametro2);
+            
+        } catch(ParametrosInvalidosException e) {
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+
+    static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException {
+        if(parametro1 >= parametro2)
+            throw new ParametrosInvalidosException();
+        
+        int contagem = parametro2 - parametro1;
+        for(int i = 0; i <= contagem; i++){
+            System.out.println(parametro1 + i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+
+}
