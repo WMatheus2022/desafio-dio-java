735bf0f4953113aae43080572fd73b68d1709dcf
WMatheus2022 <pachwander4@gmail.com>
2023-09-16 12:15:24 -0300

    modified:   bin/Contador.class 	modified:   bin/ParametrosInvalidosException.class 	modified:   src/Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -9,22 +9,21 @@ public class Contador {
         System.out.println("Valor do parametro 2: ");
         int parametro2 = terminal.nextInt();
 
-
         try {
             contar(parametro1, parametro2);
-            
-        } catch(ParametrosInvalidosException e) {
+
+        } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parametro deve ser maior que o primeiro");
         }
 
     }
 
     static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException {
-        if(parametro1 >= parametro2)
+        if (parametro1 >= parametro2)
             throw new ParametrosInvalidosException();
-        
+
         int contagem = parametro2 - parametro1;
-        for(int i = 0; i <= contagem; i++){
+        for (int i = 0; i <= contagem; i++) {
             System.out.println(parametro1 + i);
         }
     }
